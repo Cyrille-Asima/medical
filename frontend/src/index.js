8b2c50c62500a84156cf46f971058cddbcac5fa1
Cyrille Asima <casima@terpmail.umd.edu>
2023-09-13 01:10:45 -0400

    admid

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -26,6 +26,8 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import AdminRoute from './components/AdminRoute';
+import OrderListScreen from './screens/admin/OrderListScreen';
 
 
 
@@ -45,7 +47,10 @@ const router = createBrowserRouter (
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
         <Route path='/order/:id' element={<OrderScreen />} />
         <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
 
+      <Route path='' element={<AdminRoute/>}>
+        <Route path='/admin/orderlist' element={<OrderListScreen />} />
       </Route>
 
 
