33468aafb2afd0f076cd952e37295c5e4f62bd53
Cyrille Asima <casima@terpmail.umd.edu>
2023-09-27 13:19:33 -0400

    reviews

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/productController.js'
+import { createProduct,
+    deleteProduct, 
+    getProductById, 
+    getProducts, 
+    updateProduct,
+    createProductReview} from '../controllers/productController.js'
 import {protect, admin} from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
@@ -8,6 +13,7 @@ router.route('/:id')
 .get(getProductById)
 .put(protect, admin, updateProduct)
 .delete(protect, admin, deleteProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 
 
