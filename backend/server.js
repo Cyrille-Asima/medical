9cf6feb97adb198967b64b55128566356c37a8d3
Cyrille Asima <casima@terpmail.umd.edu>
2023-11-08 11:58:46 -0500

    json

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,6 @@ app.use(express.urlencoded({extended: true}))
 // cookie parser middleware
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send('Api is running...');
-});
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -39,6 +36,19 @@ res.send({clientId:process.env.PAYPAL_CLIENT_ID}))
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api is running...');
+    });
+}
 
 
 
